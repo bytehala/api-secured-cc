86023fa018b75d2bb4f93a1fd2ca51e6403a2ac5
Lem Dulfo <lemuel.dulfo@gmail.com>
2018-12-24 16:05:26 -0700

    Foundations for getting oauth

--- src/test/java/eg/bytehala/apisecuredcc/ApiSecuredCcApplicationTests.java
+++ src/test/java/eg/bytehala/apisecuredcc/ApiSecuredCcApplicationTests.java
@@ -2,16 +2,68 @@ package eg.bytehala.apisecuredcc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ApiSecuredCcApplicationTests {
+
+	@Autowired
+    private MockMvc mvc;
 
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+    public void exampleTests() throws Exception {
+	    this.mvc.perform(get("/")).andExpect(status().isOk())
+                .andExpect(content().string("Hello World"));
+    }
+
+    @Test
+    public void testClientCredentials() throws Exception {
+//	    this.mvc.perform(get)
+        obtainAccessToken("test", "test");
+    }
+
+    private String obtainAccessToken(String username, String password) throws Exception {
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+        params.add("client_id", "fooClientIdPassword");
+        params.add("username", username);
+        params.add("password", password);
+
+        ResultActions result
+                = mvc.perform(post("/oauth/token")
+                .params(params)
+                .with(httpBasic("fooClientIdPassword","secret"))
+                .accept("application/json;charset=UTF-8"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"));
+
+        String resultString = result.andReturn().getResponse().getContentAsString();
+
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        return jsonParser.parseMap(resultString).get("access_token").toString();
+    }
+
 
 }
 
