877d884cf2a57ad38e5a0a70197df7865c163299
Lem Dulfo <lemuel.dulfo@gmail.com>
2018-12-24 15:19:07 -0700

    Genesis

---
+++ src/main/java/eg/bytehala/apisecuredcc/ApiSecuredCcApplication.java
@@ -0,0 +1,14 @@
+package eg.bytehala.apisecuredcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSecuredCcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSecuredCcApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/eg/bytehala/apisecuredcc/ApiSecuredCcApplicationTests.java
@@ -0,0 +1,17 @@
+package eg.bytehala.apisecuredcc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiSecuredCcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
