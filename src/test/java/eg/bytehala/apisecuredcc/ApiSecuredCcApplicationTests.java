f9d16521d8ef0b129de02baccc7b005ef59f8818
Lem Dulfo <lemuel.dulfo@gmail.com>
2018-12-26 21:21:39 -0700

    Add index, root test works

--- src/test/java/eg/bytehala/apisecuredcc/ApiSecuredCcApplicationTests.java
+++ src/test/java/eg/bytehala/apisecuredcc/ApiSecuredCcApplicationTests.java
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+import static org.junit.Assert.assertNotNull;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -31,29 +32,44 @@ public class ApiSecuredCcApplicationTests {
 	}
 
 	@Test
-    public void exampleTests() throws Exception {
-	    this.mvc.perform(get("/")).andExpect(status().isOk())
-                .andExpect(content().string("Hello World"));
+    public void testRoot() throws Exception {
+	    this.mvc.perform(get("/")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void mustGetAToken() throws Exception {
+	    String accessToken = obtainAccessToken();
+	    assertNotNull(accessToken);
     }
 
     @Test
     public void testClientCredentials() throws Exception {
 //	    this.mvc.perform(get)
-        obtainAccessToken("test", "test");
-    }
-
-    private String obtainAccessToken(String username, String password) throws Exception {
+        String accessToken = obtainAccessToken();
 
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", "password");
-        params.add("client_id", "fooClientIdPassword");
-        params.add("username", username);
-        params.add("password", password);
+        params.add("access_token", accessToken);
+
+        this.mvc.perform(post("/api/test")
+                .params(params)
+                .accept("application/json;charset=UTF-8"))
+                .andExpect(status().isOk());
+
+    }
+
+//    private String getResultS
+
+    private String obtainAccessToken() throws Exception {
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "client_credentials");
+//        params.add("client_id", "fooClientIdPassword");
+//        params.add("client_secret", "testClientSecret");
 
         ResultActions result
                 = mvc.perform(post("/oauth/token")
                 .params(params)
-                .with(httpBasic("fooClientIdPassword","secret"))
+                .with(httpBasic("fooClientIdPassword","testClientSecret"))
                 .accept("application/json;charset=UTF-8"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json;charset=UTF-8"));
